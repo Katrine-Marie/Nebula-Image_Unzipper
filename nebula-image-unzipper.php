9135553130fec9d66fd3c8494d78c9125d615852
Katrine-Marie <fjordscene@gmail.com>
2019-02-19 10:38:55 +0100

    Base setup created

---
+++ nebula-image-unzipper.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Plugin Name: Nebula Image Unzipper
+* Description: This plugin unzips image zip files, and uploads the files into the media manager
+* Author: Katrine-Marie Burmeister
+* Version: 1.0.0
+* Author URI: https://fjordstudio.dk
+* Text Domain: nebula-unzipper
+* License:     GNU General Public License v3.0
+* License URI: https://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+if(!defined('ABSPATH')){
+	exit('Go away!');
+}
